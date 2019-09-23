408fca4f25ccb6ca4a0d65b71a36cafb368978bd
cynthiaoduol <cynthiaobu940@gmail.com>
2019-09-23 10:30:20 +0300

    move quotes array permanently to quoteComponent

---
+++ src/app/quote/quote.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import{Quote} from '../quote';
+
+@Component({
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
+})
+export class QuoteComponent implements OnInit {
+
+  quotes: Quote = [
+    {text:'Aspire to inspire before you expire', author:'Rex Hans', submitter:'Faith'},
+    {text:'Aspire to inspire before you expire', author:'Rex Hans', submitter:'Faith'},
+    {text:'Aspire to inspire before you expire', author:'Rex Hans', submitter:'Faith'}, 
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
